8e9d8c56a5a61cebffd6d53b4530ae8d90cf0358
Napu <nahuel.haberkorn@gmail.com>
2023-11-28 22:50:18 -0300

    added global cal script

--- global.js
+++ global.js
@@ -33,6 +33,46 @@ function setJWT(jwtToken) {
   localStorage.setItem("exclusiveJWT", jwtToken);
 }
 
+// Cal embed
+if(isAuth()) {
+  loadGlobalCal();
+}
+function loadGlobalCal() {
+  (function (C, A, L) {
+    let p = function (a, ar) {
+      a.q.push(ar);
+    };
+    let d = C.document;
+    C.Cal =
+      C.Cal ||
+      function () {
+        let cal = C.Cal;
+        let ar = arguments;
+        if (!cal.loaded) {
+          cal.ns = {};
+          cal.q = cal.q || [];
+          d.head.appendChild(d.createElement("script")).src = A;
+          cal.loaded = true;
+        }
+        if (ar[0] === L) {
+          const api = function () {
+            p(api, arguments);
+          };
+          const namespace = ar[1];
+          api.q = api.q || [];
+          typeof namespace === "string" ? (cal.ns[namespace] = api) && p(api, ar) : p(cal, ar);
+          return;
+        }
+        p(cal, ar);
+      };
+  })(window, "https://app.cal.com/embed/embed.js", "init");
+  Cal("init", { origin: "https://app.cal.com" });
+  Cal("inline", {
+    elementOrSelector: "[data-cal='global-embed']",
+    calLink: "exclusive",
+  });
+}
+
 // Domains
 async function getDomains() {
   try {
